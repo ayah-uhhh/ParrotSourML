2983d45004f2f91ea9edfdec21038c9df38920cf
John McCarthy <john.mccarthy.24@us.af.mil>
2023-06-09 17:08:24 +0200

    update preprocessor to reduce loops/ hard coded values

--- ParrotSourPreProcessor.py
+++ ParrotSourPreProcessor.py
@@ -15,52 +15,29 @@ from flatten_json import flatten_json
 """ IMPORT DATA """
 data = open("trainingdata\data.json")
 loaded_data = json.load(data)
-flat = loaded_data
-
-num_items = 5
-"""
-[[{'altitude': 22, 'heading': 32, 'id': 'red', 'dataTrail': {}, 'type': 0, 'startPos': {'x': 226, 'y': 93}, 'intent': {'desiredHeading': 270, 'desiredAlt': 0, 'desiredSpeed': 450, 'desiredLoc': []}, 'capping': False}
-,{'altitude': 29, 'heading': 32, 'id': 'red', 'dataTrail': {}, 'type': 0, 'startPos': {'x': 232.7843847692514, 'y': 97.23935411386564}, 'intent': {'desiredHeading': 270, 'desiredAlt': 0, 'desiredSpeed': 450, 'desiredLoc': []}, 'capping': False},
-{'altitude': 32, 'heading': 32, 'id': 'red', 'dataTrail': {}, 'type': 0, 'startPos': {'x': 239.5687695385028, 'y': 101.47870822773129}, 'intent': {'desiredHeading': 270, 'desiredAlt': 0, 'desiredSpeed': 450, 'desiredLoc': []}, 'capping': False}]]
-data is labeled as such
-0-n pictures
-each picture has 0-m groups
-each group has heading, starting position(x,y), altitude
-'n_pic' --> Label y
-data x as follows:
-['n_groups_i_j_startPos_x', 'n_groups_i_j_startPos_y',
-'n_groups_i_j_altitude', 'n_groups_i_j_heading']
-where i is the number of groups or shapes in the picture
-
-"""
 
 """Extract numerical information"""
 Y = np.transpose(np.array([]))
 
-
-# n = 0
-
 startPositions = {}
-# for key in flat:
-for k in range(len(flat)):  # hopefully being the pictures
-    for key in flat[k]:
-        groups = key.get(
-            "groups"
-        )  # stuck here. when key is in flat[k] it no longer has .get
-        startPositions[k] = {}
-        for i in range(len(groups)):  # group number in pictures
-            newx = []
-            newy = []
-            for j in range(0, len(groups[i])):  # individual points in groups
-                startPos = groups[i][j].get("startPos")
-                newx.append(startPos.get("x"))
-                newy.append(startPos.get("y"))
-                startPositions[k]["x"] += newx
-                startPositions[k]["y"] += newy
-    # n += 1
+n=0
+for k in loaded_data:  # hopefully being the pictures
+    groups = k.get("groups")  # stuck here. when key is in flat[k] it no longer has .get
+    startPositions[n] = {}
+    newx = []
+    newy = []
+    for i in range(len(groups)):  # group number in pictures
+        for j in range(0, len(groups[i])):  # individual points in groups
+            startPos = groups[i][j].get("startPos")
+            newx.append(startPos.get("x"))
+            newy.append(startPos.get("y"))
+        startPositions[n]["x"] = newx
+        startPositions[n]["y"] = newy
+    n=n+1
 
-    if "pic" in key:
-        val = key.get("pic")
+    # Make the answer key 
+    if "pic" in k:
+        val = k.get("pic")
         if "AZIMUTH" in val:
             Y = np.append(Y, "AZIMUTH")
         elif "RANGE" in val:
@@ -75,12 +52,8 @@ for k in range(len(flat)):  # hopefully being the pictures
             Y = np.append(Y, "VIC")
         elif "SINGLE" in val:
             Y = np.append(Y, "SINGLE")
-"""
-Labels
-1: Azimuth, 2: Range, 3: Wall
-4: Ladder, 5: Champagne 6: Vic
-7: Single
-"""
+
+
 
 """
 Take X1 and X2 ---> matrix of points
@@ -90,24 +63,24 @@ I want one plot for every set of points in the same group {n}
 """
 """ plot for ML """
 
-# Create a dictionary to store X1 and X2 values for each n_group
 # Create scatter plots for each n_group
 file_path = os.path.join("output", "Y.txt")
+
+if not os.path.exists("output"):
+  os.makedirs("output")
+
+open(file_path, "w+")
 img_size = (500, 500)
 save_dir = "output"
+
 np.savetxt(file_path, Y, fmt="%s")
 
-for n in range(1):
+for n in range(len(startPositions)):
     fig, ax = plt.subplots()
-    # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
     ax.scatter(startPositions[n]["x"], startPositions[n]["y"], c="black", marker="3")
     ax.set_title("")  # (f"Group {n}")
     ax.set_xlabel("")
     ax.set_ylabel("")
-    # ax.set_xlabel("X1")
-    # ax.set_ylabel("X2")
-    # ax.set_rlim(0, 100)
-    # ax.set_thetalim(0, 2*np.pi)
     ax.set_xlim(0, 400)
     ax.set_ylim(-200, 200)
     ax.spines["bottom"].set_visible(False)

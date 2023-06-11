35713fb97e2c0e1f3819a6890cb261110bd7922a
ayah <ayah.ayad333@gmail.com>
2023-06-10 19:45:22 -0500

    .

--- tfTrial.py
+++ tfTrial.py
@@ -8,24 +8,4 @@ import csv
 with open("trainingdata\start_positions.json", "r") as file:
     startPositions = json.load(file)
 
-# Define the CSV file path
-csv_file = "start_positions.csv"
-
-# Open the CSV file and write the data
-with open(csv_file, "w", newline="") as file:
-    writer = csv.writer(file)
-
-    # Write the header row
-    writer.writerow(["key", "x", "y"])
-
-    # Write the data rows
-    for key, values in startPositions.items():
-        x_values = values.get("x", [])
-        y_values = values.get("y", [])
-        writer.writerow(
-            [
-                key,
-                ",".join(str(x) for x in x_values),
-                ",".join(str(y) for y in y_values),
-            ]
-        )
+ds_file = tf.data.TextLineDataset("trainingdata\start_positions.json")

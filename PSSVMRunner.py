231d20b63ddacfc08aa7bd5e4a59cec22ea5e0b3
John McCarthy <jemccarthy13@gmail.com>
2023-06-14 20:40:37 +0200

    svm tweaks

--- PSSVMRunner.py
+++ PSSVMRunner.py
@@ -15,7 +15,7 @@ psLog.setLevel(logging.DEBUG)
 
 psLog.info("------------------------------")
 
-_, time_elapsed, error, _ = psvm.psSVM(
-    kernel="rbf", sea=100, shape="ovr", show_cm=False, save=True)
+_, time_elapsed, error, _ = psvm.psSVM(size_img=14,
+                                       kernel="rbf", sea=10, shape="ovr", show_cm=False, save=True)
 
 psLog.info("------------------------------")

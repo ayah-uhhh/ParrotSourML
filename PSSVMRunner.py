3427e58ace2d596ab9c12fc440cfcdc5831cae26
John McCarthy <jemccarthy13@gmail.com>
2023-06-14 08:06:20 +0200

    cm not in workflow

--- PSSVMRunner.py
+++ PSSVMRunner.py
@@ -15,6 +15,7 @@ psLog.setLevel(logging.DEBUG)
 
 psLog.info("------------------------------")
 
-_, time_elapsed, error = psvm.psSVM("rbf", 100, "ovr", show_cm=True, save=True)
+_, time_elapsed, error = psvm.psSVM(
+    "rbf", 100, "ovr", show_cm=False, save=True)
 
 psLog.info("------------------------------")
